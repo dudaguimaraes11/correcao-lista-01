4036a894a85653419948834084c2ac8fb63a8e8c
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-19 10:57:10 -0300

    Cria arquivo questão-04.js na pasta scripts

---
+++ scripts/questao-04.js
@@ -0,0 +1,8 @@
+const temperaturaCelcius = [0, 10, 20, 30, 40];
+let temperaturaFahrenheit = [];
+let contadorArrayFahrenheit = 0; 
+
+// Varredura de array com laço de repetição
+for (let i = 0; i < temperaturaCelcius.length; i++) {
+    temperaturaFahrenheit[contadorArrayFahrenheit] = (temperaturaCelcius [i] * 9 / 5 + 32);
+}
