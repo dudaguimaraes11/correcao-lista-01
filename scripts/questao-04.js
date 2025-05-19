1e87fe2946949dc9efdd328d950a5ac11104cf20
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-19 10:59:33 -0300

    feat: finaliza resolução da questão-04.js

--- scripts/questao-04.js
+++ scripts/questao-04.js
@@ -5,4 +5,8 @@ let contadorArrayFahrenheit = 0;
 // Varredura de array com laço de repetição
 for (let i = 0; i < temperaturaCelcius.length; i++) {
     temperaturaFahrenheit[contadorArrayFahrenheit] = (temperaturaCelcius [i] * 9 / 5 + 32);
-}
+    contadorArrayFahrenheit++
+} 
+
+console.table (temperaturaCelcius); 
+console.table (temperaturaFahrenheit); 
